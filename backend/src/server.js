d115648d27b8282904b1cc07e373e13a4e806113
Aswathy S <aswathy.s@in.abb.com>
2026-03-08 11:54:20 +0530

    added deployment code

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,12 +3,22 @@ import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';    
 const app = express();
+import path from 'path';
 dotenv.config();
 
 const PORT = process.env.PORT;
-
+const __dirname =  path.resolve();
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
+
+if(process.env.NODE_ENV==="production")
+{
+    app.use(express.static(path.join(__dirname,"../frontend/chat-app/dist")));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend/chat-app/dist/index.html"))
+    })
+}
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
