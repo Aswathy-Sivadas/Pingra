76f23888437395f29cfaf36fbcbdd24b6bdc5c02
Aswathy S <aswathy.s@in.abb.com>
2026-03-25 13:32:06 +0530

    feat: enhance authentication flow with login and logout functionality in useAuthStore

--- frontend/chat-app/src/store/useAuthStore.js
+++ frontend/chat-app/src/store/useAuthStore.js
@@ -6,6 +6,8 @@ export const useAuthStore = create((set) =>({
     authUser:null,
     isCheckingAuth: true,
     isSigningUp: false,
+    isLoggingIn: false,
+    isLoggingOut: false,
     checkAuth: async()=>{
         try{
             const res= await axiosInstance.get("/auth/check");
@@ -33,5 +35,33 @@ export const useAuthStore = create((set) =>({
         finally{
             set({isSigningUp: false})
         }
-    } 
+    },
+    login: async(data)=>{
+        set({isLoggingIn: true});
+        try{
+            const res= await axiosInstance.post("/auth/login", data);
+            set({authUser: res.data});
+            toast.success("logged in successfully!");
+        }
+        catch(error){
+            toast.error(error.response.data.message);
+        }
+        finally{
+            set({isLoggingIn: false});
+        }
+    },
+    logout: async(data)=>{
+        set({isLoggingOut:true});
+        try{
+        const res= await axiosInstance.post("/auth/logout", data);
+        toast.success("Logged out successfully!")
+        }
+        catch(error){
+            toast.error(error.response.data.message);
+            console.log("error message:",error.message)
+        }
+        finally{
+            set({isLoggingOut: false})
+        }
+    }
 }))
